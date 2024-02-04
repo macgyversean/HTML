268d933c741f52a9d6edd47ceddedc83975fe2e7
Sean Carroll <macgyversean@gmail.com>
2024-02-04 08:20:35 -0500

    added search bar absolute

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const SearchLyr = document.getElementById("SearchLyr");
   const artistName = document.getElementById("artistName");
   const lyrics1 = document.getElementById("lyrics1");
+  artistName.addEventListener("input", ArtistName);
+  lyrics1.addEventListener("input", LyricsFnc);
+  console.log(artistName);
+  console.log(lyrics1);
 
+  function LyricsFnc(e) {
+    lyrics1.textContent = e.target.value;
+  }
+
+  function ArtistName(e) {
+    artistName.textContent = e.target.value;
+  }
   lyrs.appendChild(hello);
 
   fetch("https://api.lyrics.ovh/v1/nf/hope", {
@@ -24,9 +35,14 @@ document.addEventListener("DOMContentLoaded", function () {
       return data;
     });
   function showMusicLyrics(data) {
-    const page = document.createElement("p");
-    page.textContent = data.lyrics;
-    lyrs.appendChild(page);
-    console.log(page);
+    const { lyrics } = data;
+    lyrs.innerHTML = `<pre>${lyrics}</pre>`;
+
+    // const page = document.createElement("p");
+    // page.textContent = data.lyrics;
+    // lyrs.appendChild(page);
+    // console.log(page);
   }
 });
+
+// `<pre>${data.lyrics}</pre>`
